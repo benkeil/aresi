5bd7b12a7a9fc36621282bd1d2ae973fd7d89130
Bens Keil <benkeil.me@googlemail.com>
2023-08-12 19:28:56 +0200

    Initial Commit

---
+++ aresi/aresiApp.swift
@@ -0,0 +1,17 @@
+//
+//  aresiApp.swift
+//  aresi
+//
+//  Created by Ben Keil on 12.08.23.
+//
+
+import SwiftUI
+
+@main
+struct aresiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
